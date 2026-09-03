9c9f8653a34832e3b99f08740ad2bf65647e9a99
Alp Buz <alpbuz03@outlook.com>
2026-09-03 13:36:30 -0400

    change

--- server/src/main/java/com/homeserver/Controllers/DockerController.java
+++ server/src/main/java/com/homeserver/Controllers/DockerController.java
@@ -3,7 +3,6 @@ import com.homeserver.util.docker.DockerService;
 import com.homeserver.util.docker.ContainerInfo;
 import com.homeserver.util.ApiResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
